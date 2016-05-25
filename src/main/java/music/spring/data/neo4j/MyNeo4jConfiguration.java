009bd3abf9eef9914902d81c8adc485d1843b493
João Gabriel Bracaioli <joaobracaioli@gmail.com>
2016-05-25 12:38:16 -0300

    adicionando ogm.properties

--- src/main/java/music/spring/data/neo4j/MyNeo4jConfiguration.java
+++ src/main/java/music/spring/data/neo4j/MyNeo4jConfiguration.java
@@ -1,11 +1,15 @@
 package music.spring.data.neo4j;
 
 import org.apache.log4j.Logger;
+import org.neo4j.ogm.session.Session;
 import org.neo4j.ogm.session.SessionFactory;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.data.neo4j.config.Neo4jConfiguration;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 import org.springframework.data.neo4j.server.Neo4jServer;
@@ -29,14 +33,18 @@ public class MyNeo4jConfiguration extends Neo4jConfiguration {
     public static final String URL = System.getenv("NEO4J_URL") 
     		!= null ? System.getenv("NEO4J_URL") : "http://52.40.102.85:7474";
     		
-    @Override
-    public Neo4jServer neo4jServer() {
-       return new RemoteServer(URL,"neo4j","system321");
-    }
 
     @Override
+    @Bean
     public SessionFactory getSessionFactory() {
         return new SessionFactory("music.spring.data.neo4j.domain");
     }
+
+	@Override
+	@Bean
+	@Scope(value = "session", proxyMode = ScopedProxyMode.TARGET_CLASS)
+	public Session getSession() throws Exception {
+		return super.getSession();
+	}
 }
 // end::config[]
